fe38f843e4f6b9ca5541b1947d486f005c1beec6
Samatar Axmed <Samatar_267@hotmail.com>
2017-03-16 12:35:47 +0000

    Created front-end test We've created a front-end test which checks for an error, when the first parameters passed in. Relates to issue #33

--- tests/frontendtests.js
+++ tests/frontendtests.js
@@ -7,3 +7,23 @@
 //
 //Test `input`
 //
+
+var test = require('tape');
+var handleOutput = require('../public/assets/js/handlesearch.js');
+
+
+test('Check if tape is working with simple test', function(t){
+  console.log(handleOutput);
+  t.pass(1, 1, 'Check 1 is equal to 1');
+  t.end();
+
+
+});
+
+test('Check if handleOutput returns an error when we pass in an error', function(t){
+  console.log(handleOutput);
+  t.equal(handleOutput('a'), Error, 'Yes, function returns error');
+  t.end();
+
+
+});
