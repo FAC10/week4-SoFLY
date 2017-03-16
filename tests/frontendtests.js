4d2d53354cbfc584a354ec68b0c0638be29f206c
Samatar Axmed <Samatar_267@hotmail.com>
2017-03-16 17:53:52 +0000

    Created fetch test When the apicall doesn't complete, e.g. we pass in a number, it should return a callback with the argument as true Relates to issue #55

--- tests/frontendtests.js
+++ tests/frontendtests.js
@@ -20,5 +20,11 @@ QUnit.module( 'Frontend', function (assert) {
     assert.equal(getColorCode('a'), 'There is an error', 'Yes, function returns error');
   });
 
+  QUnit.test('Check if the fetch function return a callback with the argument true, when the apicall doesnt complete', function(assert){
+
+    fetch(9, function(l){assert.equal(l, true, 'Yes the callback returns true as its argument')})
+
+  });
+
 
 });
