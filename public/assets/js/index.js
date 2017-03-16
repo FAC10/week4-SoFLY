b39fc7a22dbfa9af30914472295a6af1f4851baf
Yvonne Liu <liuyvonne.ca@gmail.com>
2017-03-16 10:45:25 +0000

    add front end tests to be tested

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,39 +1,40 @@
-
-var fetch = function (url, cb) {
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200){
-        cb(null, JSON.parse(xhr.responseText));
-      } else {
-        cb(true);
-      }
-    }
-  };
-  xhr.open('GET', url, true);
-  xhr.send();
+var fetch = function(url, cb) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                cb(null, JSON.parse(xhr.responseText));
+            } else {
+                cb(true);
+            }
+        }
+    };
+    xhr.open('GET', url, true);
+    xhr.send();
 };
 
-var searchRequest = function (val, cb) {
-  fetch('/search?q='+val, handleOutput);
+var searchRequest = function(val, cb) {
+    fetch('/search?q=' + val, handleOutput);
 
 };
 
 
 
-var input = (function () {
+var input = (function() {
 
-  var validateInput = function (inputText, cb) {
-    if (inputText && !ifSymbols(inputText)) {
-      cb(inputText);
-    }
-  };
+    var validateInput = function(inputText, cb) {
+        if (inputText && !ifSymbols(inputText)) {
+            cb(inputText);
+        }
+    };
 
-  var ifSymbols = function (string) {
-    return string.match(/[^a-z]/gi);
-  };
+    var ifSymbols = function(string) {
+        return string.match(/[^a-z]/gi);
+    };
 
-  return {validateInput:validateInput};
+    return {
+        validateInput: validateInput
+    };
 
 })();
 
@@ -41,6 +42,6 @@ var input = (function () {
 
 var inputDOM = document.getElementById('search-term');
 // DOM STUFF BELOW
-inputDOM.addEventListener('keyup', function (e) {
-  input.validateInput(e.target.value, searchRequest);
+inputDOM.addEventListener('keyup', function(e) {
+    input.validateInput(e.target.value, searchRequest);
 });
