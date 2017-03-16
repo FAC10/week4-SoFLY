972894c8ca405dbe281f265bd713d51b736f314e
Samatar Axmed <Samatar_267@hotmail.com>
2017-03-16 17:15:49 +0000

    Created QUnit tests I've created the Qunit testing structure and have put our tests in this testing framework. Relates to issue #33

--- tests/frontendtests.js
+++ tests/frontendtests.js
@@ -1,36 +1,18 @@
-//handlesearch.js
-//
-//Test when a given an object to handleOutput, it outputs option elements to the DOM
-//
-//
-//index.js
-//
-//Test `input`
-//
+QUnit.module( 'Frontend', function (assert) {
+  // write your tests here
+  QUnit.test('If 1, return 1', function(assert) {
 
-var test = require('tape');
-var handleOutput = require('../public/assets/js/handlesearch.js');
-var validateInput = require('../public/assets/js/index.js');
+    assert.equal(1, 1, 'Success: 1 does equal 1');
+  });
 
+  QUnit.test('Check if validateInput converts a string with special characters to undefined',function(assert){
+    validateInput('%^&*', function(string){assert.equal(string, undefined);});
+  });
 
-test('Check if tape is working with simple test', function(t){
-  console.log(handleOutput);
-  t.pass(1, 1, 'Check 1 is equal to 1');
-  t.end();
+  QUnit.test('Check if validateInput returns a string with alphabet characters',function(assert){
+    validateInput('Istring', function(string){assert.equal(string, 'Istring');});
+  });
+  QUnit.test('Check if handleOutput returns an error when we pass in an error', function(assert){
+    assert.equal(handleOutput('a'), Error, 'Yes, function returns error');
+  });
 });
-
-test('Check if handleOutput returns an error when we pass in an error', function(t){
-  console.log(handleOutput);
-  t.equal(handleOutput('a'), Error, 'Yes, function returns error');
-  t.end();
-});
-
-test('Check if validateInput returns a string with alphabet characters',function(t){
-  validateInput('Istring', function(string){t.equal(string, 'Istring')})
-  t.end();
-})
-
-test('Check if validateInput converts a string with special characters to undefined',function(t){
-  validateInput('%^&*', function(string){t.equal(string, undefined)})
-  t.end();
-})
