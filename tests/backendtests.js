a13bda971befdc4107e03ac05cd6f356ec22762f
Yvonne Liu <liuyvonne.ca@gmail.com>
2017-03-16 12:04:27 +0000

    added test for checking if autocomplete in router.js works
    relates to #29

--- tests/backendtests.js
+++ tests/backendtests.js
@@ -1,26 +1,39 @@
-var test = require('tape');
-var http = require('http');
+const test = require('tape');
+const http = require('http');
 
 function backendTest() {
 
+//Test router.js
+  const url = 'http://localhost:4000/';
+
   test('Check landing works', (t) => {
-    t.plan(1);
-    http.get('http://localhost:4000/', (res) => {
+    t.plan(2);
+    http.get(url, (res) => {
       t.equal(200, res.statusCode);
+      t.equal('text/html', res.headers['content-type']);
     });
   });
 
 
-//Router.js
-//
-//Test landing here
-//
-//
-//
 //Test autocomplete here
-//
-//
-//
+  test('Check autocomplete works', (t) => {
+    t.plan(3);
+    http.get(url + 'search', (res) => {
+      t.equal(200, res.statusCode);
+      t.equal('application/json', res.headers['content-type']);
+    });
+    var searchWord = 'undefined';
+    http.get(url + 'search?q=' + searchWord, (res) => {
+      var body = '';
+      res.on('data', (chunk) => {
+        body += chunk;
+      });
+      res.on('end', ()=>{
+        var { searchResults } = JSON.parse(body);
+        t.deepEqual(['undefinedly', 'undefinedness'], searchResults);
+      });
+    });
+  });
 //Test public here
 //
 //
@@ -45,7 +58,6 @@ function backendTest() {
 //
 //
 //Test serveError
-//
 //
 //
 }
